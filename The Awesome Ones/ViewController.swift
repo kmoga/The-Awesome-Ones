d4ddd1b2c85fabe0d206481a01042ddbefab8511
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-09-05 16:21:00 -0700

    Initial Commit

---
+++ The Awesome Ones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Awesome Ones
+//
+//  Created by SBAUser on 9/5/19.
+//  Copyright © 2019 Kyle Moga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
