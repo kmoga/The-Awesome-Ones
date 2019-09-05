6c48e533d76348de6534bd8d7c161c43c46a0e0a
SBAUser <sbauser@sbausers-macbook-pro.local>
2019-09-05 16:57:12 -0700

    Hope this works

--- The Awesome Ones/ViewController.swift
+++ The Awesome Ones/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewer: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    var index = -1
+    var newIndex = -1
+    var randomStart = Int.random(in: 0...7)
+    
+    var students = ["Ethan",
+                    "Braden",
+                    "Kyle",
+                    "Reece",
+                    "Max",
+                    "Jessica",
+                    "Sophia",
+                    "Jared"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        
+        if randomStart > students.count-1 {
+            randomStart = 0
+        }
+        label.text = students[randomStart]
+        viewer.image = UIImage(named: "img\(randomStart)")
+        randomStart += 1
+    }
+    
 }
 
